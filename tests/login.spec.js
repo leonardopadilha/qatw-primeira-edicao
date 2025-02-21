359b484647709600aa74e44d55c54f01d2c8ef2d
Leonardo Padilha <leonardopadilha28@gmail.com>
2025-02-21 06:16:59 -0300

    Consumindo filas do redis com o pacote bullmq

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -4,6 +4,7 @@ import { obterCodigo2FA } from '../support/db';
 import { LoginPage } from '../pages/LoginPage';
 import { DashPage } from '../pages/DashPage';
 import { LoginActions } from '../actions/LoginActions';
+import { cleanJobs, getJob } from '../support/redis'
 
 
 test('Não deve logar quando o código de validação é inválido', async ({ page }) => {
@@ -36,16 +37,19 @@ test('Deve acessar a conta do usuário', async ({ page }) => {
     cpf: '00000014141',
     senha: '147258'
   }
+
+  await cleanJobs()
   
   await loginPage.informaCpf(usuario.cpf)
   await loginPage.informaSenha(usuario.senha)
 
-  await page.waitForTimeout(10000)
-  const codigo = await obterCodigo2FA('00000014141')
-  await loginPage.informa2FA(codigo)
+   // Checkpoint
+   await page.getByRole('heading', { name: 'Verificação em duas etapas' }).waitFor({timeout: 8000})
 
-  // Checkpoint
-  await page.getByRole('heading', { name: 'Verificação em duas etapas' }).waitFor({timeout: 8000})
+   const codigo = await getJob()
+
+  //const codigo = await obterCodigo2FA(usuario.cpf)
+  await loginPage.informa2FA(codigo)
 
   await expect(await dashPage.obterSaldo()).toHaveText('R$ 5.000,00')
 });
@@ -67,7 +71,7 @@ test('Deve acessar a conta do usuário utilizando actions', async ({ page }) => 
   // Checkpoint
   await page.getByRole('heading', { name: 'Verificação em duas etapas' }).waitFor({timeout: 8000})
 
-  const codigo = await obterCodigo2FA('00000014141')
+  const codigo = await obterCodigo2FA(usuario.cpf)
   await loginActions.informa2FA(codigo)
 
   await expect(await loginActions.obterSaldo()).toHaveText('R$ 5.000,00')
